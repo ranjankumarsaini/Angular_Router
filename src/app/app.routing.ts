21d97872a5e382d735a06cd0e729998f7879eb0b
unknown <ranjankumarsaini@gmail.com>
2019-01-31 22:20:30 +0530

    First Commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import { CalcComponent } from './calc/calc.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './notfound/notfound.component';
+
+export const AppRoutes: Routes = [
+	{ path: '', component: HomeComponent },
+    { path: 'calc', component: CalcComponent },
+    // { path: '**', component: NotFoundComponent }
+    { path: '**', redirectTo:'' }
+];
+
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
